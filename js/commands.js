224d1b0513068776d4659306a7c20f4f4be39e23
ParkaD4 <138149432+ParkaD4@users.noreply.github.com>
2025-06-19 16:43:46 -0300

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,148 @@
+var wallpaper = "https://media.discordapp.net/attachments/905415464962916432/1385137134813450282/image.png?ex=6854f8fb&is=6853a77b&hm=f38a2f1a0a0c6fa4e5589b97d3bc29c69059c4371f456f1fdf81f0c17a197e8b&=&format=webp&quality=lossless";
+var bailao = "https://www.youtube.com/watch?v=hOl2e0PZzqo";
+var confidencial = "https://www.youtube.com/watch?v=FtutLA63Cp8&pp=ygUJYmFkIGFwcGxl";
+var luan = "https://www.youtube.com/watch?v=qjjT960FYt4";
+var alheio = "https://www.youtube.com/watch?v=sO2UJ7elDy0";
+
+dadosatuais = [
+  "<br>",
+  'Nível de Patente: <span class="liam">Vice-Comandante</span> da Nave Mãe.',
+  "Posição de Existência: Sexto.",
+  "Status: Vivo.",
+  "Localização Atual: █████████.",
+  "Missão Atual: Infiltrado em meio a humanos para maior adaptação, obtenção de informações e detecção de possíveis cobaias.",
+  "Último Contato com a Nave Mãe: 01 ano e 03 meses.",
+  "<br>"
+];
+
+navemae = [
+  "<br>",
+  'Transmissão fracassada. O sinal está sendo bloqueado por <span class="tungstenio">algo</span> nas proximidades.',
+  "<br>"
+];
+
+catálogo = [
+  "<br>",
+  '<span class="liam">Liam Willwood</span>',
+  "Médico Chefe - Mark Neil",
+  "Enfermeira - Sarah June",
+  "Zelador - Harris Ethan",
+  "Atendente - Diego Ezra",
+  '<span class="estanho">Agente Estanho - Azura Gael </span>',
+  '<span class="chumbo">Agente Chumbo - Caspian Graham </span>',
+  '<span class="tungstenio">Agente Tungstênio - Jake Skayl </span>',
+  '<span class="platina">Agente Platina - Telbrieg Yaldr </span>',
+  "<br>"
+];
+
+seita = [
+  "<br>",
+  'A <span class="magistrada">Magistrada</span> e sua Seita já está ciente da Nave Mãe e de parte dos experimentos, incluindo os do Hospital Woodings Greyhouse.',
+  'Com base nos dados recolhidos, tais ações podem acarretar em um futuro confronto. Foi ordenado por nosso <span class="liam">Comandante</span> que preparativos sejam feitos.',
+  "Novas armas especializadas em contra-medidas para lidar com a Seita são requisitadas imediatamente, sem direito a objeção.",
+    "<br>"
+];
+
+locaisdeinteresse = [
+  "<br>",
+  '<span class="elevador">████ ████</span> Acesso negado.',
+  '<span class="magistrada">ꂠ̴̛̛̺͓͇̰̦̦̫̒̔̌́͌̑ꂑ̶̫̥͎͙͖͚̯͒̀̾ꋊ̸̧̛̭̣̫̈͗͗̆̓̕͠ꁲ̸̡͙̣͙̤͉̟̲̖͠ꂵ̴̳͙̟̲͈̬̘̭̤̺̋̎͛̽͐̊̆ꁲ̴̧̼͖̳̩͊ꌅ̷͓͙̖̣̎̽͑͂̈̚͜ꀯ̶͍͎̼͖̙̮̎̉͘̕ͅꁲ̴͍͌</span> Arquivo corrompido.',
+  '<span class="texas">███████ █████ ██ █████</span> Acesso negado.',
+  '<span class="anfitras">Inglaterra</span> Insira o comando de acesso para maior informações.',
+  '<span class="ddm">███████████</span> Acesso negado.',
+  '<span class="guiana">██████ ████████</span> Acesso negado.',
+  '<span class="oceano">oɔıɟıɔɐԀ ouɐǝɔO</span> Insira o comando de acesso para maior informações.',
+  '<span class="australia">█████████</span> Acesso negado.',
+  '<span class="diabo">███████████</span> Acesso negado.',
+  "<br>"
+];
+
+projetos = [
+  "<br>",
+  '<span class="derretido">∂єяяєтι∂σ</span>',
+  "Finalidade: Arma biológica capaz de assumir formas diversas para infiltração, com capacidade de consumir matéria orgânica para futura evolução.",
+  'Componentes: <span class="chumbo">Agente Chumbo - Caspian Graham</span>, Médico Chefe - Mark Neil, Zelador - Harris Ethan.',
+  "Desfecho do Experimento: Fracasso.",
+  "Status Atual: Ativo.",
+  "ㅤ",
+  '<span class="mescla">ᗰᗴᔕᑕᒪᗩ</span>',
+  "Finalidade: Criação de um assassino habilidoso, capaz de eliminar silenciosamente e multiplicar-se sem intervenção dos alheios.",
+  'Componentes: <span class="platina">Agente Platina - Telbrieg Yaldr</span>.',
+  "Desfecho do Experimento: Fracasso.",
+  "Status Atual: Ativo.",
+  "ㅤ",
+  '<span class="infecticideo">Ìñ£ê¢†ï¢íÐêð</span>',
+  "Finalidade: Meio de apagar dados, destruir tecnologia inimiga e eliminar alvos em áreas inalcançaveis ou desconhecidas.",
+  'Componentes: <span class="platina">Agente Estanho - Azura Gael</span>, Enfermeira - Sarah June, Atendente - Diego Ezra.',
+  "Desfecho do Experimento: Em analise.",
+  "Status Atual: Ativo.",
+  "<br>"
+];
+
+inglaterra = [
+  "<br>",
+  "Relatos apontam uma 'cidade fantasma' na região de Londres, coexistindo dentro da capital de forma sigilosa e paranormal.",
+  'Os boatos locais revelam lendas sobre desaparecimentos nas redondezas, pessoas fantasmas que nunca existiram e mortos, cuja única marca deixada é uma incomum sombra no local.',
+  "Novas informações precisam ser adquiridas.",
+  'Possíveis causas: Objeto amaldiçoado fora de controle, criatura paranormal com habilidades interessantes, <span class="anfitras">⍭⍾⍒☍∳⍽☍a ⌥⍾ ⍾⋨⍾⍭⌹☍a</span>.',
+    "<br>"
+];
+
+oceanopacífico = [
+  "<br>",
+  "Detectamos uma atividade incomum na região oeste do Oceano Pacifico. Investigações revelaram a existência de uma base submersa em profundezas aquáticas.",
+  'Vibrações anormais foram observadas próximas ao local. Agentes da Ordem aparentam estar trabalhando em conjunto com outra organização, que aparenta não saber de sua real função.',
+  "Acidentes nos locais adjacentes a origem da vibração relatam algo se movimentando dentre as profundezas marinhas.",
+  "Uma investigação mais aprofundada já foi requesitada.",
+    "<br>"
+];
+
+climatização = [
+  "<br>",
+  "Por questões de segurança todos os climatizadores permanecerão ativos a todo momento, não sendo recomendado sua desativação.",
+  'É possível mudar o seu modo, de <span class="frio">frio</span> para <span class="calor">calor</span> e vice-versa caso for necessário ou requisitado.',
+  "Para facilitação da identificação do modo ativo no momento, a tabela de climatizadores ativos alternará sua coloração conforme a temperatura da área.",
+  '<span class="command">Ala Esquerda:</span> <span class="calor">Ativo</span>.',
+  '<span class="command">Ala Direita:</span> <span class="calor">Ativo</span>.',
+  '<span class="command">Ala Superior</span> <span class="calor">Ativo</span>.',
+  '<span class="command">Ala Inferior:</span> <span class="calor">Ativo</span>.',
+  '<span class="command">Sala S-612:</span> <span class="calor">Ativo</span>.',
+  '<span class="command">Sala E-███:</span> <span class="frio">Acesso Negado</span>.',
+  "<br>"
+];
+
+eletricidade = [
+  "<br>",
+  '<span class="eletricidade">Geradores em atividade:</span> <span class="infecticideo">01/██</span>.',
+  "Devido a natureza volátil das atuais circunstâncias, foram desativados cerca de ██ geradores locais. Afim de prevenir futuros danos as salas de interesse.",
+  "Em caso de retomada do Hospital Woodings Greyhouse como base de operações, será necessário religar cada gerador gradualmente, liberando acesso a outras áreas.",
+  "Caso algum gerador seja comprometido, componentes para a restauração de apenas uma unidade estão localizadas na sala 09.",
+  "<br>"
+];
+
+comandos = [
+  "<br>",
+  '<span class="command">dadosatuais</span>    Dados atuais do Vice-Comandante.',
+  '<span class="command">navemae</span>        Transmissão com a Nave Mãe.',
+  '<span class="command">climatização</span>   Explicação sobre o controle de clima e umidade das proximidades.',
+  '<span class="command">catálogo</span>       Catálogo de Vítimas.',
+  '<span class="command">█████████████████</span>      ⍭⍾⍒☍∳⍽☍a⎅.',
+  '<span class="command">projetos</span>       Informações avançadas sobre os recentes projetos.',
+  '<span class="command">confidencial</span>   Arquivo de extremo sigilo.',
+  '<span class="command">eletricidade</span>   Resumo breve do manuseamento da energia do local.',
+  '<span class="command">seita</span>          Status atual em relação a Seita das Máscaras.',
+  '<span class="command">limpar</span>         Limpa quaisquer caracteres no Terminal.',
+  "ㅤ",
+  '<span class="mescla">Por motivos de segurança os demais comandos foram ocultados. Por favor, utilize-os por conta própria </span> <span class="liam">Seis</span><span class="mescla">.</span>',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Conexão bem sucedida, sincronização com a base de dados local iniciada.</span>',
+  " ▄▄▄▄· ▄▄▄ .• ▌ ▄ ·.     ▌ ▐·▪   ▐ ▄ ·▄▄▄▄            ·▄▄▄▄  ▄▄▄ .     ▌ ▐·      ▄▄▌  ▄▄▄▄▄ ▄▄▄·     .▄▄ · ▄▄▄ .▪  .▄▄ ·    ",
+  " ▐█ ▀█▪▀▄.▀··██ ▐███     █·█▌██ •█▌▐███▪ ██ ▪         ██▪ ██ ▀▄.▀·    ▪█·█▌▪     ██•  •██  ▐█ ▀█     ▐█ ▀. ▀▄.▀·██ ▐█ ▀.    ",
+  " ▐█▀▀█▄▐▀▀▪▄▐█ ▌▐▌▐█·▪▪▪▐█▐█•▐█·▐█▐▐▌▐█· ▐█▌ ▄█▀▄     ▐█· ▐█▌▐▀▀▪▄    ▐█▐█• ▄█▀▄ ██▪   ▐█.▪▄█▀▀█     ▄▀▀▀█▄▐▀▀▪▄▐█·▄▀▀▀█▄   ",
+  " ██▄▪▐█▐█▄▄▌██ ██▌▐█▌    ███ ▐█▌██▐█▌██. ██ ▐█▌.▐▌    ██. ██ ▐█▄▄▌     ███ ▐█▌.▐▌▐█▌▐▌ ▐█▌·▐█ ▪▐▌    ▐█▄▪▐█▐█▄▄▌▐█▌▐█▄▪▐█   ",
+  " ·▀▀▀▀  ▀▀▀ ▀▀  █▪▀▀▀.    ▀  ▀▀▀▀▀ █▪▀▀▀▀▀•  ▀█▄▀▪    ▀▀▀▀▀•  ▀▀▀     . ▀   ▀█▄▀▪.▀▀▀  ▀▀▀  ▀  ▀  ▀    ▀▀▀▀  ▀▀▀ ▀▀▀ ▀▀▀▀  ▀ ",
+  "<span class=\"color2\">Para uma lista de comandos ativos, digite</span> <span class=\"command\">'comandos'</span><span class=\"color2\">.</span>",
+];
